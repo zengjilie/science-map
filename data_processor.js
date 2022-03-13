1840cc496b029e6b7eeb2413750b0700ea4fb773
zengjilie <zengjilie@gmail.com>
2022-03-13 12:53:19 -0500

    updated processor

---
+++ data_processor.js
@@ -0,0 +1,41 @@
+const csv = require('csvtojson');
+const fs = require('fs');
+
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    const a = 0.5;
+    return `rgba(${r},${g},${b},${a})`;
+
+}
+
+//Read csv file
+csv()
+    .fromFile(__dirname + '/10_conference_tsne.csv')
+    .then((data) => {
+        var stream = fs.createWriteStream('10_data.json', { flags: 'w' });
+        var arr = [];
+        //CSV -> JSON
+
+        const map = new Map();
+
+        data.forEach((entry) => {
+
+            //add color to label
+            if (map.has(entry.label)) {
+                entry.color = map.get(entry.label);
+            } else {
+                const newColor = getRandomColor();
+                // console.log(newColor);
+                map.set(entry.label, newColor);
+                entry.color = newColor;
+            }
+
+            arr.push(entry);
+        })
+
+        const json = { data: arr };
+
+        stream.write(JSON.stringify(json));
+    })
--- threemap.js
+++ threemap.js
@@ -1,6 +1,6 @@
 import * as THREE from './node_modules/three/build/three.module.js';
 import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
-
+import data from './Pro'
 //scene
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xcccccc);
